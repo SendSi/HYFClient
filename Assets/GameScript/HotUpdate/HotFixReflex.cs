406f95939def20809478f5a00ab50118d869459f
曾思信 <zengsixin@manlinggame.com>
2024-01-10 15:54:04 +0800

    123 加入浏海屏  Screen.safeArea

--- Assets/GameScript/HotUpdate/HotFixReflex.cs
+++ Assets/GameScript/HotUpdate/HotFixReflex.cs
@@ -7,9 +7,13 @@ public class HotFixReflex
     public static void Run()
     {
         Debug.LogWarning("HotFixReflex-->Run");
+        var gameMain = GameObject.Find("GameMain");//Find元素
+
         ProxyLoginModule.Instance.OpenLoginMainView();
         ProtocalBinder.BindAll();
         ManagerBinder.BindAll();
+        
+        if (gameMain != null) { gameMain.AddComponent<SafeAreaUtils>(); }
     }
 
     public static void Destroy()
