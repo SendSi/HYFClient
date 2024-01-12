dae5e47a6c8c0e4990e80ad65a972f44c06ce8c9
曾思信 <zengsixin@manlinggame.com>
2024-01-12 19:03:28 +0800

    音效添加   声音播放 AudioMgr.cs 播放背景PlayBGM(),播放音效PlayMusic() 后续做成导表形式吧
    # Conflicts:
    #	Assets/GameScript/AOT/AppConfig.cs
    #	README.md

--- Assets/GameScript/AOT/AppConfig.cs
+++ Assets/GameScript/AOT/AppConfig.cs
@@ -1,22 +1,26 @@
 public static class AppConfig
 {
-    public static string serverURL = "https://192.168.15.20:5001";//服务器地址
+    public static string serverURL = "https://192.168.15.20:5001"; //服务器地址
     // public static string serverURL = "https://localhost:5001";//服务器地址
     // public static string serverURL = "https://tylearymf.familyds.com:8888";//服务器地址
-    
-    public static string hostServerIP = "http://192.168.15.20";//cdn资源服务器
+
+    public static string hostServerIP = "http://192.168.15.20"; //cdn资源服务器
     // public static string hostServerIP = "http://127.0.0.1"; //cdn资源服务器
-    
+
     public static string appVersion = "v1.0"; //打exe时(apk时) 的版本
 
-    public static int designResolutionX = 1334;//ui设计尺寸 宽
-    public static int designResolutionY= 750;//ui设计尺寸 高
-    
-    public static string currLang = "SimChinese";//当前出APK时的语言 简体中文SimChinese  繁体中文TraChinese  英文English          //初次启动  改变后再次启动
+    public static int designResolutionX = 1334; //ui设计尺寸 宽
+    public static int designResolutionY = 750; //ui设计尺寸 高
+
+    public static string currLang = "SimChinese"; //当前出APK时的语言 简体中文SimChinese  繁体中文TraChinese  英文English          //初次启动  改变后再次启动
+
+    public static float bgmVolume = 0.66f; //bgm的默认声音大小  
+    public static float musicVolume = 0.66f; //音效的默认声音大小  
 
     #region editor下使用的
-    public static string resVersion = "v1.0"; //yooAsset生成资源时 的版本  复制时有用  editor下使用
-    public static string localCDN = "D:/WWW_hyfclient/CDN/";//本地cdn资源服务器 磁盘路径
-    #endregion
 
+    public static string resVersion = "v1.0"; //yooAsset生成资源时 的版本  复制时有用  editor下使用
+    public static string localCDN = "D:/WWW_hyfclient/CDN/"; //本地cdn资源服务器 磁盘路径
+
+    #endregion
 }
--- README.md
+++ README.md
@@ -28,4 +28,10 @@ yooAsset学习_hybridCLR学习_fairyGUI学习
     特效使用 EffectLoader.cs 加载UI特效使用LoadUIEffect()或LoadUIEffectEPos(),加载场景特效使用LoadSceneEffectSimple()或LoadSceneEffect(),,非auto的记得释放,估计后续得扩展,得用传入导表id的形式进行加载吧
     导表使用 在根目录处有Excel与excel2json_tool,导出时用DictObject类型的json文件,使用ConfigMgr.cs的LoadConfigOne<ItemConfig>("id")取得一行数据
     浏海屏幕 SafeAreaUtils.cs处理了左右横屏,制作fgui工程时需注意一下大背景得左右两边往外伸一点即可
-    多国语言 LanguageUtils.cs可查看文件头部注释.导表映射.fgui内置字..切换语言时,会退出应用.重启生效
+	多国语言 LanguageUtils.cs可查看文件头部注释.导表映射.fgui内置字..切换语言时,会退出应用.重启生效
+	声音播放 AudioMgr.cs 播放背景PlayBGM(),播放音效PlayMusic() 后续做成导表形式吧
+
+####  服务端
+    使用gRPC进行通信协议 测试了exe是正常热更的,,,,无需服务端,则使用另一分支noServer
+    HYFServer要与HYFClient同一个文件夹下 (Toolkit\ProtoGen.bat有定义路径)
+    在UnityEditor下有菜单栏.直接使用proto即可生成客户端用的代码
