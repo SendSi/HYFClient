1eac1eabb2ce2f1a931530aad87e709ac81339d2
曾思信 <389585111@qq.com>
2024-02-20 20:38:46 +0800

    测试

---
+++ Assets/GameScript/HotUpdate/Logic/Welfare/MenuItemWelfare.cs
@@ -0,0 +1,13 @@
+﻿using FairyGUI;
+
+namespace Welfare
+{
+    public partial class MenuItemWelfare : GButton
+    {
+        public void SetData(MenuData cfg)//data字段不能被赋值
+        {
+            this.title = cfg._name;
+            this._tagCtrl.selectedIndex = 0;
+        }
+    }
+}
--- Assets/GameScript/HotUpdate/Logic/Welfare/WelfareMainView.cs
+++ Assets/GameScript/HotUpdate/Logic/Welfare/WelfareMainView.cs
@@ -1,30 +1,70 @@
+using System.Collections.Generic;
 using FairyGUI;
 using UnityEngine;
 
 namespace Welfare
 {
+    public class MenuData
+    {
+        public int _id { get; set; }
+        public string _name { get; set; }
+        public int _type { get; set; } //0分类   1具体活动
+
+        public MenuData(int id, string name, int type)
+        {
+            this._id = id;
+            this._name = name;
+            this._type = type;
+        }
+    }
+
     public partial class WelfareMainView : GComponent
     {
+        private List<MenuData> mMenuDtos= new List<MenuData>()
+        {
+            new MenuData(1000,"新手活动",0),
+            new MenuData(1001,"成长基金",1),
+            new MenuData(1002,"首充礼包",1),
+            new MenuData(2000,"日常活动",0),
+            new MenuData(2001,"每日特惠",1),
+            new MenuData(2002,"每周特惠",1),
+        };
         public override void OnInit()
         {
             base.OnInit();
 
+           
             this._closeButton.onClick.Set(() => { ProxyWelfareModule.Instance.CloseWelfareMainView(); });
 
             this._leftTabList.itemRenderer = OnRendererTabList;
             this._leftTabList.itemProvider = OnProviderTabList;
+            this._leftTabList.onClickItem.Add(OnClickItemLeftTab);
+        }
 
+        private void OnClickItemLeftTab()
+        {
         }
 
         private string OnProviderTabList(int index)
         {
-            return "MenuItemWelfare";
-            return "MenuTypeWelfare";
+            return  mMenuDtos[index]._type==0?"MenuTypeWelfare":"MenuItemWelfare";
         }
 
-        private void OnRendererTabList(int index, GObject item)
+        private void OnRendererTabList(int index, GObject obj)
         {
-            
+            var cfg = mMenuDtos[index];
+            if (cfg._type==0)
+            {
+                MenuTypeWelfare item = (MenuTypeWelfare)obj;
+                item.SetData(mMenuDtos[index]);
+            }
+            else
+            {
+                MenuItemWelfare item = (MenuItemWelfare)obj;
+                item.SetData(mMenuDtos[index]);
+            }
+
+            obj.data = cfg;
         }
 
         public override void Dispose()
@@ -35,6 +75,7 @@ namespace Welfare
         public void SetData(string value)
         {
             Debug.LogError("TTTT");
+            this._leftTabList.numItems = 6;// mMenuDtos.Count;
         }
     }
 }
