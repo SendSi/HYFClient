b7a95576139a803a95dfa6c603c5191928e7252f
曾思信 <389585111@qq.com>
2024-02-22 20:13:44 +0800

    小修改

---
+++ Assets/GameScript/HotUpdate/Config/cs/TodayGiftConfig.cs
@@ -0,0 +1,19 @@
+//
+// Auto Generated Code By excel2json
+// https://neil3d.gitee.io/coding/excel2json.html
+// 1. 每个 Sheet 形成一个 Struct 定义, Sheet 的名称作为 Struct 的名称
+// 2. 表格约定：第一行是变量名称，第二行是变量类型
+
+// Generate From TodayGiftConfig.xlsx
+
+public class TodayGiftConfig
+{
+	public int id; // ID
+	public string titleTxt; // 中间描述
+	public string descTxt; // 补充描述
+	public string commodity; // 商品
+	public int iconCtrl; // 宝箱图标
+}
+
+
+// End of Auto Generated Code
