e0f908a5ee469739861a0cff7e4e34d564cad941
曾思信 <389585111@qq.com>
2025-02-10 21:01:23 +0800

    测试
    # Conflicts:
    #	Assets/GameRes/fgui/PuzzlePKG_fui.bytes
    #	Assets/GameScript/HotUpdate/UIGen/PuzzlePKG/PuzzleMainView.cs
    #	Assets/GameScript/HotUpdate/UIGen/PuzzlePKG/PuzzlePKGBinder.cs
    #	UIProject/assets/PuzzlePKG/components/Item_Puzzle.xml
    #	UIProject/assets/PuzzlePKG/package.xml
    #	UIProject/assets/PuzzlePKG/panel/PuzzleMainView.xml

---
+++ Assets/GameScript/HotUpdate/UIGen/PuzzlePKG/PuzzleMainView.cs
@@ -0,0 +1,129 @@
+/** This is an automatically generated class by FairyGUI. Please do not modify it. **/
+
+using FairyGUI;
+using FairyGUI.Utils;
+
+namespace PuzzlePKG
+{
+    public partial class PuzzleMainView : GComponent
+    {
+        public GButton _closeButton;
+        public GLoader _iconBg;
+        public GButton _btnGoto;
+        public GButton _10;
+        public GButton _11;
+        public GButton _12;
+        public GButton _13;
+        public GButton _14;
+        public GButton _15;
+        public GButton _20;
+        public GButton _21;
+        public GButton _22;
+        public GButton _23;
+        public GButton _24;
+        public GButton _25;
+        public GButton _30;
+        public GButton _31;
+        public GButton _32;
+        public GButton _33;
+        public GButton _34;
+        public GButton _35;
+        public GButton _40;
+        public GButton _41;
+        public GButton _42;
+        public GButton _43;
+        public GButton _44;
+        public GButton _45;
+        public GGroup _bgTop;
+        public Item_Puzzle _110;
+        public Item_Puzzle _111;
+        public Item_Puzzle _112;
+        public Item_Puzzle _113;
+        public Item_Puzzle _114;
+        public Item_Puzzle _115;
+        public Item_Puzzle _120;
+        public Item_Puzzle _121;
+        public Item_Puzzle _122;
+        public Item_Puzzle _123;
+        public Item_Puzzle _124;
+        public Item_Puzzle _125;
+        public Item_Puzzle _130;
+        public Item_Puzzle _131;
+        public Item_Puzzle _132;
+        public Item_Puzzle _133;
+        public Item_Puzzle _134;
+        public Item_Puzzle _135;
+        public Item_Puzzle _140;
+        public Item_Puzzle _141;
+        public Item_Puzzle _142;
+        public Item_Puzzle _143;
+        public Item_Puzzle _144;
+        public Item_Puzzle _145;
+        public GGroup _frontTop;
+        public const string URL = "ui://791wrm7st16bl7";
+
+        public static PuzzleMainView CreateInstance()
+        {
+            return (PuzzleMainView)UIPackage.CreateObject("PuzzlePKG", "PuzzleMainView");
+        }
+
+        public override void ConstructFromXML(XML xml)
+        {
+            base.ConstructFromXML(xml);
+
+            _closeButton = (GButton)GetChild("closeButton");
+            _iconBg = (GLoader)GetChild("iconBg");
+            _btnGoto = (GButton)GetChild("btnGoto");
+            _10 = (GButton)GetChild("10");
+            _11 = (GButton)GetChild("11");
+            _12 = (GButton)GetChild("12");
+            _13 = (GButton)GetChild("13");
+            _14 = (GButton)GetChild("14");
+            _15 = (GButton)GetChild("15");
+            _20 = (GButton)GetChild("20");
+            _21 = (GButton)GetChild("21");
+            _22 = (GButton)GetChild("22");
+            _23 = (GButton)GetChild("23");
+            _24 = (GButton)GetChild("24");
+            _25 = (GButton)GetChild("25");
+            _30 = (GButton)GetChild("30");
+            _31 = (GButton)GetChild("31");
+            _32 = (GButton)GetChild("32");
+            _33 = (GButton)GetChild("33");
+            _34 = (GButton)GetChild("34");
+            _35 = (GButton)GetChild("35");
+            _40 = (GButton)GetChild("40");
+            _41 = (GButton)GetChild("41");
+            _42 = (GButton)GetChild("42");
+            _43 = (GButton)GetChild("43");
+            _44 = (GButton)GetChild("44");
+            _45 = (GButton)GetChild("45");
+            _bgTop = (GGroup)GetChild("bgTop");
+            _110 = (Item_Puzzle)GetChild("110");
+            _111 = (Item_Puzzle)GetChild("111");
+            _112 = (Item_Puzzle)GetChild("112");
+            _113 = (Item_Puzzle)GetChild("113");
+            _114 = (Item_Puzzle)GetChild("114");
+            _115 = (Item_Puzzle)GetChild("115");
+            _120 = (Item_Puzzle)GetChild("120");
+            _121 = (Item_Puzzle)GetChild("121");
+            _122 = (Item_Puzzle)GetChild("122");
+            _123 = (Item_Puzzle)GetChild("123");
+            _124 = (Item_Puzzle)GetChild("124");
+            _125 = (Item_Puzzle)GetChild("125");
+            _130 = (Item_Puzzle)GetChild("130");
+            _131 = (Item_Puzzle)GetChild("131");
+            _132 = (Item_Puzzle)GetChild("132");
+            _133 = (Item_Puzzle)GetChild("133");
+            _134 = (Item_Puzzle)GetChild("134");
+            _135 = (Item_Puzzle)GetChild("135");
+            _140 = (Item_Puzzle)GetChild("140");
+            _141 = (Item_Puzzle)GetChild("141");
+            _142 = (Item_Puzzle)GetChild("142");
+            _143 = (Item_Puzzle)GetChild("143");
+            _144 = (Item_Puzzle)GetChild("144");
+            _145 = (Item_Puzzle)GetChild("145");
+            _frontTop = (GGroup)GetChild("frontTop");
+        }
+    }
+}
---
+++ Assets/GameScript/HotUpdate/UIGen/PuzzlePKG/PuzzlePKGBinder.cs
@@ -0,0 +1,16 @@
+/** This is an automatically generated class by FairyGUI. Please do not modify it. **/
+
+using FairyGUI;
+
+namespace PuzzlePKG
+{
+    public class PuzzlePKGBinder
+    {
+        public static void BindAll()
+        {
+            UIObjectFactory.SetPackageItemExtension(Item_Puzzle.URL, typeof(Item_Puzzle));
+            UIObjectFactory.SetPackageItemExtension(PuzzleMainView_bak.URL, typeof(PuzzleMainView_bak));
+            UIObjectFactory.SetPackageItemExtension(PuzzleMainView.URL, typeof(PuzzleMainView));
+        }
+    }
+}
