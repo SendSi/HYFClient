fd71705a72e2205caa49aeffd866bfb0ac58c08d
曾思信 <389585111@qq.com>
2025-02-13 20:37:39 +0800

    测试

--- Assets/GameScript/HotUpdate/Logic/HitHamsterPKG/HitHamsterPKGManager.cs
+++ Assets/GameScript/HotUpdate/Logic/HitHamsterPKG/HitHamsterPKGManager.cs
@@ -1,6 +1,53 @@
 ﻿public class HitHamsterPKGManager : Singleton<HitHamsterPKGManager>
 {
     private int mSumTime = 300; //总时间 30秒
+    private int mHp = 5;
+    private int mScore = 0;
+    protected override void OnInit()
+    {
+        base.OnInit();
+        mSumTime = 300; //总时间 30秒
+        mHp = 5;
+        mScore = 0;
+    }
+
+    public void AddTime(int oneTime)
+    {
+        mSumTime += oneTime;
+    }
+
+    public int GetSumTime()
+    {
+        return mSumTime;
+    }
+
+    public void AddHp(int oneHp)
+    {
+        mHp += oneHp;
+    }
+
+    public int GetHp()
+    {
+        return mHp;
+    }
+
+    public void AddScore(int oneScore)
+    {
+        mScore += oneScore;
+    }
+
+    public int GetScore()
+    {
+        return mScore;
+    }
+
+    protected override void OnDispose()
+    {
+        base.OnDispose();
+        mSumTime = 300; //总时间 30秒
+        mHp = 5;
+        mScore = 0;
+    }
     
     
 }
--- Assets/GameScript/HotUpdate/Logic/PokerPKG/PokerManager.cs
+++ Assets/GameScript/HotUpdate/Logic/PokerPKG/PokerManager.cs
@@ -1,20 +1,18 @@
 ﻿using System.Collections.Generic;
 using cfg;
+
 public class PokerManager : Singleton<PokerManager>
 {
-    Dictionary<int, List<PokerConfig>>
-        mPokerPlayerDic = new Dictionary<int, List<PokerConfig>>(); //key是玩家id,,,,value是手牌
-
+    Dictionary<int, List<PokerConfig>> mPokerPlayerDic = new Dictionary<int, List<PokerConfig>>(); //key是玩家id,,,,value是手牌
     private List<PokerConfig> mPokerAllLists;
 
     public void ListenPoker()
     {
     }
 
-
     public List<PokerConfig> GetSelfCardList(int playerId = 1)
     {
-        mPokerAllLists =  CfgLubanMgr.Instance.globalTab.TbPokerConfig.DataList;//ConfigMgr.Instance.LoadConfigList<PokerConfig>();
+        mPokerAllLists = CfgLubanMgr.Instance.globalTab.TbPokerConfig.DataList; //ConfigMgr.Instance.LoadConfigList<PokerConfig>();
         mPokerAllLists = OtherUtils.Instance.GetRandomList(mPokerAllLists); //打乱排序
 
         for (int i = 1; i < 4; i++)
@@ -26,13 +24,16 @@ public class PokerManager : Singleton<PokerManager>
         for (int i = 0; i < 52; i++)
         {
             if (ply == 4) ply = 1;
-            
+
             mPokerPlayerDic[ply].Add(mPokerAllLists[i]);
 
             ply++;
         }
 
-
         return mPokerPlayerDic[playerId];
     }
 }
+// 单张  对子  三张
+//三带一  三带二  顺子 连对  飞机  飞机带翅膀
+//四带两单   四带两对
+//普炸弹  王炸
